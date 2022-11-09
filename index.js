5aeb0a8f3b3d1f8c03a0e23d7739601186b14aae
Md. Junaidul Islam <info.junaidulislam@gmail.com>
2022-11-09 18:21:50 +0600

    confidential data hidden

--- index.js
+++ index.js
@@ -2,8 +2,34 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
+app.use(express.json());
+
+require("dotenv").config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@jscluster.n9s8s9n.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+async function run(){
+    try{
+        const database = client.db("fixPC").collection("services");
+        const query = {};
+        const cursor = await database.find(query);
+
+        
+    }
+    finally{
+        //nothing
+    }
+}
+
+run().catch(error => console.error(error));
 
 app.get("/", (req, res) => {
     res.send("FixPC Server is running...");
